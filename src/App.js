f9c2ec69580899ecb155f706ac827f24265f1260
Hyungsuk Kim <76229107+sesame0717@users.noreply.github.com>
2021-07-28 14:02:34 -0700

    Saving progress

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
+import Signin from './Components/Signin/Signin';
+import Register from './Components/Register/Register';
 import './App.css';
 
 // Clarifai API
@@ -33,37 +35,81 @@ class App extends Component {
     this.state = {
       input: '',
       imgURL: '',
+      box: {},
+      route: 'signin'
     }
   }
 
+  // calculates location of face
+  // @param: data 
+  //        response from Clarifai API predict
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    };
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box });
+  }
+
+  // Detect button event
+  // calls Clarifai API and displays the result
   onButtonSubmit = () => {
-    this.setState({ imgURL: this.state.input });
+    //  resets box if there were any previous calculations
+    this.setState({ imgURL: this.state.input, box: {} });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch((err) => {
         console.log('Something is wrong', err);
       })
   }
 
+  // Input event
+  // changes state of App
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   }
 
+  onRouteChange = (route) => {
+    this.setState({ route })
+  }
+
+  on
+
   render() {
     return (
       <div className="App">
-        <Particles className='particles' params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imgURL={this.state.imgURL}/>
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation onRouteChange={this.onRouteChange} />
+        {this.state.route === 'home' ?
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={this.state.box} imgURL={this.state.imgURL} />
+          </div>
+          :  (
+            this.state.route === 'signin' ?
+              <Signin onRouteChange={this.onRouteChange} />
+              : <Register onRouteChange={this.onRouteChange}/>
+          )
+        }
       </div>
     );
   }
--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imgURL }) => {
+const FaceRecognition = ({ imgURL, box }) => {
     return (
-        <div className="center">
-            <img src={imgURL} alt='face'></img>
+        <div className="center ma mv2">
+            <div className= 'absolute'>
+                <img id='inputImage' src={imgURL} alt='' width='500px' heigth='auto'></img>
+                <div
+                    className='bounding-box'
+                    style={{ top: box.topRow, bottom: box.bottomRow, left: box.leftCol, right: box.rightCol }}>
+                </div>
+            </div>
         </div>
     );
 }
